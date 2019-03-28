e514be44a53d77d838541baecf31113d372c5cc9
grabstacker <grabstacker@users.noreply.github.com>
2019-03-28 19:30:10 +1300

    The base classes in place for the PC builder application

---
+++ InheritanceProtecionModifiers/Computer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProtecionModifiers
+{
+    //Type of Techonology to confirgure
+    class Computer
+    {
+    }
+
+    //Choose the type of pc to Configure
+    public class PC
+    {
+
+    }
+
+    public class Mac
+    {
+
+    }
+
+    //Choose operating system
+
+    public class Windows
+    {
+
+    }
+
+    public class Unix
+    {
+
+    }
+
+
+    public class OSX
+    {
+
+    }
+
+    //After the user has picked its base  platforms we can choose 
+    //The different hardware options for budget.
+    public class hardware
+    {
+
+    }
+
+
+}
