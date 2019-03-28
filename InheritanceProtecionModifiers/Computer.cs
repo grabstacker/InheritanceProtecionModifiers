2c4fb29f7e85f167719d700ad7d3835bcba23563
grabstacker <grabstacker@users.noreply.github.com>
2019-03-28 19:48:06 +1300

    Added all the inheritance classes and setup information variables that are protected. Plus a class to Configure and build the pc

--- InheritanceProtecionModifiers/Computer.cs
+++ InheritanceProtecionModifiers/Computer.cs
@@ -7,44 +7,67 @@ using System.Threading.Tasks;
 namespace InheritanceProtecionModifiers
 {
     //Type of Techonology to confirgure
-    class Computer
+    public class Computer
     {
     }
 
     //Choose the type of pc to Configure
-    public class PC
+    public class PC : Computer
     {
-
+        protected String Brand;
     }
 
-    public class Mac
+    public class Mac : Computer
     {
-
+        protected String Brand;
     }
 
     //Choose operating system
 
-    public class Windows
+    public class Windows : PC
     {
-
+        protected Decimal Version;
     }
 
-    public class Unix
+    public class Unix : Mac
     {
-
+        protected Decimal Version;
     }
 
 
-    public class OSX
+    public class OSX : Mac
     {
-
+        protected Decimal Version;
     }
 
     //After the user has picked its base  platforms we can choose 
     //The different hardware options for budget.
-    public class hardware
+    public class hardware : Windows 
+    {
+        protected Decimal smallCost;
+        protected Decimal mediumCost;
+        protected Decimal largeCost;
+
+        
+    }
+
+    // Combines all information for processing and printing the order 
+    // to screen
+    public class BuildPc : hardware
     {
 
+
+        //Prints the entire Configuration to screen
+        public void ConfigfurationPrint()
+        {
+
+        }
+
+        //Notifies the user of the Current status of their order
+        public void CurrentStatus()
+        {
+
+        }
     }
 
 
